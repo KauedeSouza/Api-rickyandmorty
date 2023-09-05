972a6e92f6d6ae1a76a371faa48d6f79ea9efa28
Kauê de Souza <114521349+KauedeSouza@users.noreply.github.com>
2023-09-05 10:28:34 -0300

    tarefa feita

--- API-rickyandmorty/js/script.js
+++ API-rickyandmorty/js/script.js
@@ -1,31 +1,188 @@
+// Função para buscar todos os personagens
 
-function buscar(){
-   id =  document.getElementById("id").value
+function buscarTodos() {
 
-   const itemTable = document.getElementById("itemTable").getElementsByTagName('tbody')[0];
-   // Fazer a requisição HTTP
-   fetch("https://rickandmortyapi.com/api/character/"+id)
-       .then(response => response.json())
-       .then(data => {
-           console.log(data);
-           
-           const newRow = itemTable.insertRow();
-           const idCell = newRow.insertCell(0);
-           const nomeCell = newRow.insertCell(1);
-           const statusCell = newRow.insertCell(2);
-           const especieCell = newRow.insertCell(3);
-           const tipoCell = newRow.insertCell(4);
+    const itemTable = document.getElementById("itemTable").getElementsByTagName('tbody')[0];
 
-           idCell.innerHTML = data.id;
-           nomeCell.innerHTML = data.name;
-           statusCell.innerHTML = data.status;
-           especieCell.innerHTML = data.species;
-           tipoCell.innerHTML = data.type;
-       })
-       .catch(error => {
-           console.error("Erro na requisição:", error);
-       });
+    itemTable.innerHTML = ""; // Limpa a tabela antes de preenchê-la novamente
 
 
+
+    const genderFilter = document.getElementById("gender").value;
+
+    const statusFilter = document.getElementById("status").value;
+
+
+
+    // Função para buscar personagens de uma página específica com filtros aplicados
+
+    function buscarPersonagensPagina(url) {
+
+        fetch(url)
+
+            .then(response => response.json())
+
+            .then(data => {
+
+                console.log(data.results);
+
+
+
+                // Preenche a tabela com os personagens da página atual que atendem aos filtros
+
+                data.results.forEach(character => {
+
+                    if ((genderFilter === "" || character.gender === genderFilter) &&
+
+                        (statusFilter === "" || character.status === statusFilter)) {
+
+                        const newRow = itemTable.insertRow();
+
+                        const idCell = newRow.insertCell(0);
+
+                        const nomeCell = newRow.insertCell(1);
+
+                        const statusCell = newRow.insertCell(2);
+
+                        const generoCell = newRow.insertCell(3);
+
+                        const especieCell = newRow.insertCell(4);
+
+
+
+
+                        idCell.innerHTML = character.id;
+
+                        nomeCell.innerHTML = character.name;
+
+                        statusCell.innerHTML = character.status;
+
+                        generoCell.innerHTML = character.gender;
+
+                        especieCell.innerHTML = character.species;
+
+
+
+                    }
+
+                });
+
+
+
+                // Verifica se há mais páginas e busca a próxima página se existir
+
+                if (data.info.next) {
+
+                    buscarPersonagensPagina(data.info.next);
+
+                }
+
+            })
+
+            .catch(error => {
+
+                console.error("Erro na requisição:", error);
+
+            });
 
     }
+
+
+
+    // Inicia a busca a partir da primeira página com filtros aplicados
+
+    buscarPersonagensPagina("https://rickandmortyapi.com/api/character/");
+
+}
+
+
+
+// Função para buscar por ID
+
+function buscarPorId() {
+
+    const id = document.getElementById("id").value;
+
+    const itemTable = document.getElementById("itemTable").getElementsByTagName('tbody')[0];
+
+    itemTable.innerHTML = ""; // Limpa a tabela antes de preenchê-la novamente
+
+
+
+    // Fazer a requisição HTTP para um personagem específico
+
+    fetch("https://rickandmortyapi.com/api/character/" + id)
+
+        .then(response => response.json())
+
+        .then(character => {
+
+            console.log(character);
+
+
+
+            // Preenche a tabela com o personagem encontrado
+
+            const newRow = itemTable.insertRow();
+
+            const idCell = newRow.insertCell(0);
+
+            const nomeCell = newRow.insertCell(1);
+
+            const statusCell = newRow.insertCell(2);
+
+            const generoCell = newRow.insertCell(3);
+
+            const especieCell = newRow.insertCell(4);
+
+
+
+
+            idCell.innerHTML = character.id;
+
+            nomeCell.innerHTML = character.name;
+
+            statusCell.innerHTML = character.status;
+
+            generoCell.innerHTML = character.gender;
+
+            especieCell.innerHTML = character.species;
+
+
+
+
+        })
+
+        .catch(error => {
+
+            console.error("Erro na requisição:", error);
+
+        });
+
+}
+
+
+
+// Função para aplicar o filtro quando houver mudança nos menus suspensos
+
+function aplicarFiltro() {
+
+    // Recupere os valores selecionados nos menus suspensos
+
+    const genderFilter = document.getElementById("gender").value;
+
+    const statusFilter = document.getElementById("status").value;
+
+
+
+    // Chame uma função para buscar personagens com base nos filtros selecionados
+
+    buscarTodos();
+
+}
+
+
+
+// Chama a função para buscar todos os personagens ao carregar a página
+
+buscarTodos();
